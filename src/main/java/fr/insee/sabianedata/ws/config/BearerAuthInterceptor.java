c50eba466b31bab4395462af1873c5cb5028469f
Simon Demaziere <simon.demaziere@insee.fr>
2025-04-03 14:42:56 +0200

    fix: Json headers in requests

--- src/main/java/fr/insee/sabianedata/ws/config/BearerAuthInterceptor.java
+++ src/main/java/fr/insee/sabianedata/ws/config/BearerAuthInterceptor.java
@@ -1,7 +1,9 @@
 package fr.insee.sabianedata.ws.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -27,6 +30,10 @@ public class BearerAuthInterceptor implements ClientHttpRequestInterceptor {
 				request.getHeaders().setBearerAuth(token);
 			}
 		}
+
+		HttpHeaders headers = request.getHeaders();
+		headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+		headers.setContentType(MediaType.APPLICATION_JSON);
 
 		return execution.execute(request, body);
 	}
